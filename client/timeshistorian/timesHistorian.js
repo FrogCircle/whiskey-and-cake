900d7820e48ebe47f1b32969b7139a3fcbdc3331
Eddie Dolan <dolan.charles06@gmail.com>
2015-03-26 18:53:41 -0500

    Added backend for times historian and connected to front end

--- client/timeshistorian/timesHistorian.js
+++ client/timeshistorian/timesHistorian.js
@@ -1,15 +1,17 @@
 // Helper functions for player-hand-view.html
 
-Template.playerHand.helpers({
+Template.timesHistorian.helpers({
 
 
 });
 
 // player-hand-view.html template event listeners
-Template.playerHand.events({
-
-
-
+Template.timesHistorian.events({
+    "click .timesButton": function (event) {
+      Meteor.call('getTimesData', function(err, id){
+        console.log(id, 'resultId');
+      });
+    }
 });
 
 
