f9e74c4a379f2d23ebf01abcda6431f58cbac032
drceric <drc.ericlau@gmail.com>
2015-03-26 14:18:52 -0400

    server bugs fixed

--- server/fixtures.js
+++ server/fixtures.js
@@ -8,7 +8,8 @@ var PlayerHand = {};
 var GameBoard = {};
 var RoundInfo = {};
 
-var CreateRoom = function() {
+Meteor.methods ({
+  CreateRoom: function() {
   var WhiteDeck = [];
   var BlackDeck = [];
 // in-place shuffle algorithm for BlackCards
@@ -45,7 +46,48 @@ var CreateRoom = function() {
     GameBoard: [],
     users: []
   });
-};
+
+  return Room._id;
+}});
+
+// var CreateRoom = function() {
+//   var WhiteDeck = [];
+//   var BlackDeck = [];
+// // in-place shuffle algorithm for BlackCards
+// // cards are shuffled prior to instantiating the database
+//   for (var i = 0; i < BlackCards.length; i++) {
+//     var j = Math.floor(Math.random() * i);
+//     var hole = BlackCards[i];
+//     BlackCards[i] = BlackCards[j];
+//     BlackCards[j] = hole;
+//   }
+
+// // instantiate databases with shuffled data
+//   for (var i = 0; i < BlackCards.length; i++) {
+//     BlackDeck.push({
+//       text: BlackCards[i]["text"],
+//       expansion: BlackCards[i]["expansion"]
+//     });
+//   }
+
+//   for (var i = 0; i < WhiteCards.length; i++) {
+//     WhiteDeck.push({
+//       text: WhiteCards[i]["text"],
+//       expansion: WhiteCards[i]["expansion"]
+//     });
+//   }
+
+//   Room.insert({
+//     createdBy:(Meteor.userId()),
+//     createdAt: new Date(),
+//     WhiteDeck: WhiteDeck,
+//     BlackDeck: BlackDeck,
+//     RoundInfo: {},
+//     PlayerHand: [],
+//     GameBoard: [],
+//     users: []
+//   });
+// };
 
 // fields added to Meteor.user on instantiation
 Accounts.onCreateUser(function(options, user) {
@@ -59,22 +101,19 @@ Accounts.onCreateUser(function(options, user) {
 Meteor.publish("Room", function() {
   return Room.find();
 });
-Meteor.publish("WhiteDeck", function() {
-  return WhiteDeck.find();
+Meteor.publish("WhiteDeck", function(roomID) {
+  return Room.find( {}, { WhiteDeck: 1 } );
 });
 Meteor.publish("BlackDeck", function() {
-  return BlackDeck.find();
+  return Room.find( {}, { BlackDeck: 1 } );
 });
-Meteor.publish("PlayerHand", function() {
-  return PlayerHand.find({owner: this.userId});
+Meteor.publish("User", function() {
+  return Room.find( {}, { User: 1 } );
 });
 Meteor.publish("GameBoard", function() {
-  return GameBoard.find();
-});
-Meteor.publish("userData", function() {
-  return Meteor.users.find()
+  return Room.find( {}, { GameBoard: 1 } );
 });
 Meteor.publish("RoundInfo", function() {
-  return RoundInfo.find()
+  return Room.find( {}, { RoundInfo: 1 } );
 });
 
